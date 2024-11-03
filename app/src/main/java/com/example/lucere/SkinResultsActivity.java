855675772cdbcc6a8ea9e9a248c334dbfd8f1ee9
yasmin halabi <you@example.com>
2024-11-03 19:12:17 +0200

    added the skinresults page after the camera

---
+++ app/src/main/java/com/example/lucere/SkinResultsActivity.java
@@ -0,0 +1,20 @@
+package com.example.lucere;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SkinResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skinresults);  // Load the skinresults layout
+    }
+
+    // You can remove this method for now if it's not needed
+    private void openProfilePage() {
+        Intent intent = new Intent(this, SetUpProfileActivity.class);
+        startActivity(intent);
+    }
+}
