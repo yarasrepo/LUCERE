9c7dc63e6aedda96c8831e2b95500b319ec6a570
nmt29 <nmt29@mail.aub.edu>
2024-11-03 14:22:41 +0200

    Made Eczema diagnosis page

---
+++ app/src/main/java/com/example/lucere/DiagnosisActivity.java
@@ -0,0 +1,36 @@
+package com.example.lucere;
+
+//import android.content.Intent;
+import android.os.Bundle;
+//import android.widget.ImageButton;
+//import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class DiagnosisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnosis);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*ImageButton profileButton = findViewById(R.id.toolbar_icon);
+        profileButton.setOnClickListener(v -> {
+            // Create an Intent to navigate to the ProfileActivity
+            Intent intent = new Intent(DiagnosisActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });*/
+
+        /*TextView ingredientButton = findViewById(R.id.ingredient_btn);
+        ingredientButton.setOnClickListener(v -> {
+                // Create an Intent to navigate to the Salicylic Acid Activity (file name to be changed)
+                Intent intent = new Intent(DiagnosisActivity.this, SalicylicAcidActivity.class);
+                startActivity(intent);
+        });*/
+    }
+}
+
