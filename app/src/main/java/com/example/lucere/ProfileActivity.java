5145f68d3d0aac67e8daba56dcb44954a71ffd5e
Staleee <145343918+Staleee@users.noreply.github.com>
2024-11-03 20:23:22 +0200

    added profile to main

---
+++ app/src/main/java/com/example/lucere/ProfileActivity.java
@@ -0,0 +1,25 @@
+package com.example.lucere;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Button editProfileButton = findViewById(R.id.editProfileButton);
+
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
